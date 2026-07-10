1bd74ca7a86c3a891f068064b66bd43b54d54a37
dabid <293522862+dav-voidd@users.noreply.github.com>
2026-07-10 10:10:56 +0800

    Update stack8_final.py

--- stack8_final.py
+++ stack8_final.py
@@ -8,18 +8,13 @@ class Stack:
     def push(self, name):
         if len(self.stack) < 5:
             self.stack.append(name)
+            print(f"{self.stack}")
         elif len(self.stack) == 5:
             print("Stack overflow!")
 
-
-    def count(self):
-        return self.stack
-
-    def __str__(self):
-        return str(self.stack)
 
 obj = Stack()
 while True:
         user_input = input("Put item: ")
         obj.push(user_input)
-        print(obj.count())
+
