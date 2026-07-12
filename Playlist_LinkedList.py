9ab92c64ea6dc0c355a8779c1b2e00c2534cf97b
dabid <293522862+dav-voidd@users.noreply.github.com>
2026-07-12 19:36:05 +0800

    Add files via upload

---
+++ Playlist_LinkedList.py
@@ -0,0 +1,59 @@
+#The Music Playlist Shuffler (Linked List)
+
+
+
+class SongNode:
+    def __init__(self, song_title, artist):
+        self.song_title = song_title
+        self.artist = artist
+        self.next = None
+
+class Playlist:
+    def __init__(self):
+        self.current = None
+
+    def add_song(self, song, artist):
+        new_song = SongNode(song, artist)
+        if self.current is None:
+            self.current = new_song
+            return
+
+        current = self.current
+        while current.next is not None:
+            current = current.next
+        current.next = new_song
+
+    def skip_song(self):
+        if self.current is None:
+            print("The playlist is empty add more songs!")
+            return
+
+        if self.current.next is None:
+            print(f"Finished playing: {self.current.song_title}. No more songs left!")
+            self.current = None
+            return
+
+        print(f"Skipping: {self.current.song_title}")
+        self.current = self.current.next
+        print(f"Now playing: {self.current.song_title}")
+
+    def play_all(self):
+        current_song = self.current
+        while current_song is not None:
+            print(f"Now playing: [{current_song.song_title}] by {current_song.artist}")
+            current_song = current_song.next
+        print("No more songs")
+
+playlist = Playlist()
+while True:
+    user_input = input("Type (p) to play all, (s) to skip song, (a) to add song: ")
+    if user_input == 'p':
+        playlist.play_all()
+    elif user_input == 's':
+        playlist.skip_song()
+    elif user_input == 'a':
+        user_song = input("Whats the name of the song: ")
+        user_artist = input("Whats the name of the artist?: ")
+        playlist.add_song(user_song, user_artist)
+    else:
+        print("Invalid")
