2b2c72994c957890f65beca354ff0d1b13fbbdd3
dabid <293522862+dav-voidd@users.noreply.github.com>
2026-07-12 20:49:44 +0800

    Add files via upload

---
+++ Webpage_LinkedListNode.py
@@ -0,0 +1,46 @@
+#Webpage Node
+from http.cookiejar import cut_port_re
+
+
+class PageNode:
+    def __init__(self, url):
+        self.url  = url
+        self.next = None
+        self.prev = None
+
+class BrowserHistory:
+    def __init__(self):
+        self.current_page = None
+
+    def visit_page(self, url):
+        new_page = PageNode(url)
+        if self.current_page is None:
+            self.current_page = new_page
+            print(f"Visited: {self.current_page.url}")
+            return
+
+        self.current_page.next = new_page
+        new_page.prev = self.current_page
+        self.current_page = new_page
+        print(f"Visited: {self.current_page.url}")
+
+    def go_back(self):
+        if self.current_page.prev is not None:
+            self.current_page = self.current_page.prev
+            print(f"{self.current_page.url}")
+        else:
+            print("No previous page")
+
+
+Web = BrowserHistory()
+while True:
+    user_input = input("(g) go back, Type to Add and visit a URL: ")
+    if user_input == 'g':
+        Web.go_back()
+    else:
+        Web.visit_page(user_input)
+
+
+
+
+
